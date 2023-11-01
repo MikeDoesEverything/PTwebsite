994e1f9d5c7b3dfb14d5ac89e5320501404d8256
Mike Williamson <mwilliamson94@hotmail.co.uk>
2023-11-01 14:00:34 +0000

    editing the style of the webpage across all pages

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.homepage)
+    path("", views.homepage),
+    path("contact_us/", views.contact_us)
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "homepage.html")
+
+
+def contact_us(request):
+    return render(request, "contact_us.html")
