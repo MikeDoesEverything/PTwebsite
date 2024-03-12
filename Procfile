40c805032df72e19c94a4a2bf2e55d2e2b1376d1
Mike Williamson <mwilliamson94@hotmail.co.uk>
2024-03-12 11:17:15 +0000

    Setup for heroku deployment and added requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pthomepage.wsgi
--- pthomepage/settings.py
+++ pthomepage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
