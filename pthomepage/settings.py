fa79aab144636da55e127464e3c7e61890450383
Mike Williamson <mwilliamson94@hotmail.co.uk>
2024-04-17 14:52:24 +0100

    Updated security

--- pthomepage/settings.py
+++ pthomepage/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u6^s(!juda+r@t$3fh_)zdhz(3j*fd#)0b0_^99$f4*c#89520'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
